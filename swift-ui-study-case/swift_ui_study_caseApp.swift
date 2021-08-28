a19580b1ea9af0840426770e61ae4d2008808599
Eric Ampire <ericampire.top@gmail.com>
2021-08-28 20:49:55 +0200

    Initial Commit

---
+++ swift-ui-study-case/swift_ui_study_caseApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_study_caseApp.swift
+//  swift-ui-study-case
+//
+//  Created by Guest on 28/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_study_caseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
